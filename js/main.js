8598238c0c72ab7fcb31dce8f0f24eb91ad43cd7
John Doe <johndoe@example.com>
2020-10-20 21:53:50 +0300

    viking

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(function(){
+    $('.menu__btn').on('click', function (){
+        $('.menu__list').toggleClass('menu__list--active')
+    });
+
+
+
+    $('[data-fancybox]').fancybox({
+        youtube : {
+            controls : 1,
+            showinfo : 1,
+        }
+
+    });
+
+    $('.heroes__slider-img').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.heroes__slider-text',
+        prevArrow: '<button  class="slick-btn slick-prev"><img src="images/arrow-left.png" alt=""></button>',
+        nextArrow: '<button  class="slick-btn slick-next"><img src="images/arrow-right.png" alt=""></button>',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    arrows:false
+
+                }
+            }
+            ]
+    });
+
+    $('.heroes__slider-text').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        asNavFor: '.heroes__slider-img',
+        arrows:false
+    });
+});
